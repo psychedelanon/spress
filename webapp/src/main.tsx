ef2803e48835f9c28187e74fc8eb47d5161bafec
psychedelanon <store2222222real@gmail.com>
2025-07-16 21:50:14 -0400

    Add Coinbase MiniKit integration and manifest

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MiniKitProvider projectId="spress-base-demo">
+      <App />
+    </MiniKitProvider>
   </StrictMode>,
 )
